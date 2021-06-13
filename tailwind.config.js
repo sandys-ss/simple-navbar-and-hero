8d92e9568069d4be1418ab1d4312b25f6000e4b7
sandys-ss <sandysyam@gmail.com>
2021-06-13 11:24:45 +0800

    production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  // set enabled true for production, false for normal (lin2 2-8)
   purge: {
     enabled: false,
     content: [
-      './dist/index.html'
+      './dist/*.html',
+      './dist/*.js'
     ]
   },
   darkMode: false, // or 'media' or 'class'
